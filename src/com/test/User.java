920754e70b12ed2f5e58b0e32eeaeb73fb8e3bad
Administrator <824102061@qq.com>
2020-05-19 14:36:23 +0800

    Default Changelist

--- src/com/test/User.java
+++ src/com/test/User.java
@@ -9,4 +9,8 @@ public class User {
     private String name2;
     private int age=1;
     private int age1=1;
+
+    public static void main(String[] args) {
+        System.out.println("1234");
+    }
 }
